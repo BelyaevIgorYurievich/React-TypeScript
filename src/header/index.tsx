d7f238daab811ee39fe7b49fbe2901623068e2bc
Igor <justhiigor@yandex.ru>
2018-05-26 16:27:49 +0300

    Исправляет сорку проекта добавляет элементы из material-ui

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
+import * as Material from 'material-ui';
+import { MuiThemeProvider, lightBaseTheme } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+//
+// TODO Выести в отдельный модуть! 
+// Неохоимо переиспользовать!
+//
+const lightMuiTheme = getMuiTheme(lightBaseTheme);
 
 interface IAppProps {
     value: string;
     handleChangeValue: (event: {})=>void;
 }
 
-export const Header = (props:IAppProps) =>{
+export const Header = (props:IAppProps) => {
+
     return (
-        <input 
-            value={ props.value }
-            onChange={ props.handleChangeValue }
+        <MuiThemeProvider muiTheme={lightMuiTheme}>
+            <Material.TextField
+              value={ props.value }
+              onChange={ props.handleChangeValue }
+            />
+            <Material.TextField
+          multiLine={true}
+          rows={2}
+          rowsMax={4}
         />
+        </MuiThemeProvider>
     )
 } 
